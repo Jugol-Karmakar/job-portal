2c72fd950b11f2d226fe3895a71bf6096c372ad5
JugolKarmakar <jugol.cse@gmail.com>
2022-11-06 22:28:05 +0600

    job commit

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const userRoute = require("./routes/user.route");
+const adminRoute = require("./routes/admin.route");
+const generalRoute = require("./routes/general.route");
+const candidateRoute = require("./routes/candidate.route");
+const hiringManagerRoute = require("./routes/hiringManager.route");
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/", generalRoute);
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/", candidateRoute);
+app.use("/api/", hiringManagerRoute);
+
+require("./config/dbConfig");
 
 app.get("/", (req, res) => {
   res.send("Job Portal");
